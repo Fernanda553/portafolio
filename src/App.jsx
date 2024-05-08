826ab1d53bc775fa9babf8e913e3bda7e6286bed
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-08 14:38:33 -0400

    basic portfolio structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
+      <h1>Fernanda Nuñez</h1>
+      <p>Aquí debe ir la fotografía</p>
+      <h2>Descripción</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Aquí deben ir los proyectos</p>
+        <p>informacíon de mis paginas web</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Aquí el footer</p>
     </>
   )
 }
