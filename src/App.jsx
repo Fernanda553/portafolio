67d5fe1fb61b6d8bb921c46b334b9823a273bd53
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-10 16:43:12 -0400

    add description

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ const App = () => {
       <h1>Fernanda Nuñez</h1>
       <p>Aquí debe ir la fotografía</p>
       <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
-      <h2>Descripción</h2>
+      <h2>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
+        enfocada tanto en la eficiencia del código como en la experiencia del usuario.
+
+        Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
+        valoradas, y en las que pueda contribuir a través de la implementación de soluciones
+        innovadoras
+      </h2>
       <div>
         <p>Aquí deben ir los proyectos</p>
         <p>informacíon de mis paginas web</p>
