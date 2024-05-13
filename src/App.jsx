37135fa2ff7464a6c2b6d2edeb6fefd17f6c9fe9
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-13 16:00:07 -0400

    changed page structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import './App.css'
 const App = () => {
   return (
     <>
-      <h1>Fernanda Nuñez</h1>
-      <p>Aquí debe ir la fotografía</p>
-      <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
-      <h2>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
-        enfocada tanto en la eficiencia del código como en la experiencia del usuario.
-
-        Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
-        valoradas, y en las que pueda contribuir a través de la implementación de soluciones
-        innovadoras
-      </h2>
-      <div>
-        <p>Aquí deben ir los proyectos</p>
-        <p>informacíon de mis paginas web</p>
+      <div className='presentacion'>
+        <h1>Desarolladora Full Stack</h1>
+        <p>Fernanda Nuñez</p>
+        <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
       </div>
-      <p>Aquí el footer</p>
+      <div className='descripcion-websites'>
+        <h2>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
+          enfocada tanto en la eficiencia del código como en la experiencia del usuario.
+
+          Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
+          valoradas, y en las que pueda contribuir a través de la implementación de soluciones
+          innovadoras
+        </h2>
+        <i class='fa-brands fa-linkedin' />
+        <i class='fa-brands fa-github' />
+      </div>
+      <div className='stack-tecnologico'>
+        <p>Aquí debe ir el stack tecnologico</p>
+      </div>
+      <div className='proyectos'>
+        <p>Aquí deben ir los proyectos</p>
+      </div>
+      <div className='contacto'>
+        <p>contactame</p>
+      </div>
     </>
   )
 }
