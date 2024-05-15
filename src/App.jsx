f980444e800917def5a1aab8a89118f369d92c12
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-14 21:34:32 -0400

    add basic styles for sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ const App = () => {
     <>
       <div className='presentacion'>
         <h1>Desarolladora Full Stack</h1>
-        <p>Fernanda Nuñez</p>
+        <h2>Fernanda Nuñez</h2>
         <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
       </div>
       <div className='descripcion-websites'>
-        <h2>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
+        <h3>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
           enfocada tanto en la eficiencia del código como en la experiencia del usuario.
 
           Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
           valoradas, y en las que pueda contribuir a través de la implementación de soluciones
           innovadoras
-        </h2>
-        <i class='fa-brands fa-linkedin' />
-        <i class='fa-brands fa-github' />
+        </h3>
+        <i class='fa-brands fa-linkedin fa-3x' />
+        <i class='fa-brands fa-github fa-3x' />
       </div>
       <div className='stack-tecnologico'>
         <p>Aquí debe ir el stack tecnologico</p>
