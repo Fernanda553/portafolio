f2747cf7b7c2502c746ddbb8c2f6b4800a16d3fc
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-05-17 15:31:37 -0400

    add links for sections in Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Navigation } from './components/Navigation'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
       <Navigation />
-      <div className='presentacion'>
-        <h1>Desarolladora Full Stack</h1>
-        <h2>Fernanda Nuñez</h2>
-        <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
-        <h3>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
-          enfocada tanto en la eficiencia del código como en la experiencia del usuario.
+      <Container fluid className='p-0'>
+        <section className='presentacion'>
+          <h1>Desarolladora Full Stack</h1>
+          <h2>Fernanda Nuñez</h2>
+          <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
+          <h3>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
+            enfocada tanto en la eficiencia del código como en la experiencia del usuario.
 
-          Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
-          valoradas, y en las que pueda contribuir a través de la implementación de soluciones
-          innovadoras
-        </h3>
-      </div>
-      <div className='stack-tecnologico'>
-        <p>Aquí debe ir el stack tecnologico</p>
-      </div>
-      <div className='proyectos'>
-        <p>Aquí deben ir los proyectos</p>
-      </div>
-      <div className='contacto'>
-        <p>contactame</p>
-      </div>
+            Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
+            valoradas, y en las que pueda contribuir a través de la implementación de soluciones
+            innovadoras
+          </h3>
+        </section>
+        <section id='tecnologias' className='stack-tecnologico'>
+          <p>Aquí debe ir el stack tecnologico</p>
+        </section>
+        <section id='proyectos' className='proyectos'>
+          <p>Aquí deben ir los proyectos</p>
+        </section>
+        <section id='contactame' className='contacto'>
+          <p>contactame</p>
+        </section>
+      </Container>
     </>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-scroll'
 
 export const Navigation = () => {
   return (
     <Navbar bg='dark' data-bs-theme='dark' className='fixed-top'>
       <Container fluid>
+        <Nav className='me-auto'>
+          <Nav.Link as={Link} to='tecnologias' smooth duration={100}>
+            <p className='navigation'>Tecnologías</p>
+          </Nav.Link>
+          <Nav.Link as={Link} to='proyectos' smooth duration={100}>
+            <p className='navigation'>Proyectos</p>
+          </Nav.Link>
+          <Nav.Link as={Link} to='contactame' smooth duration={100}>
+            <p className='navigation'>Contáctame</p>
+          </Nav.Link>
+        </Nav>
         <Nav className='ms-auto'>
           <Nav.Link
             href='https://www.linkedin.com/in/fernandanunezespinoza/'
             target='_blank'
             rel='noopener noreferrer'
           >
-            <i class='navigation fa-brands fa-linkedin fa-lg' />
+            <i class='icons fa-brands fa-linkedin fa-lg' />
           </Nav.Link>
           <Nav.Link
             href='https://github.com/Fernanda553'
             target='_blank'
             rel='noopener noreferrer'
           >
-            <i class='navigation fa-brands fa-github fa-lg' />
+            <i class='icons fa-brands fa-github fa-lg' />
           </Nav.Link>
         </Nav>
       </Container>
