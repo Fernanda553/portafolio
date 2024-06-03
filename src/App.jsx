434ca23b723199bc9df7f8592e64a1d12cbaea2a
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-06-03 17:24:58 -0400

    add meta in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,14 @@ const App = () => {
         <section id='introduccion' className='presentacion'>
           <h1>Desarolladora Full Stack</h1>
           <h2>Fernanda Nuñez</h2>
-          <img alt='Fernanda' src='./src/assets/img/fernanda.jpeg' />
-          <h3>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
+          <img alt='Fernanda fotografía' src='./src/assets/img/fernanda.jpeg' />
+          <p>Desarrolladora FullStack con una sólida formación en el Desarrollo de Aplicaciones Web y
             enfocada tanto en la eficiencia del código como en la experiencia del usuario.
 
             Actualmente busco espacios de desarrollo donde mi creatividad y habilidades técnicas sean
             valoradas, y en las que pueda contribuir a través de la implementación de soluciones
             innovadoras
-          </h3>
+          </p>
         </section>
         <section id='tecnologias' className='stack-tecnologico'>
           <p>Aquí debe ir el stack tecnologico</p>
